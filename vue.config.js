2484b6635a61132d3bd44687af712b32e85af44f
L.Dragon <121632587@qq.com>
2020-07-06 16:22:29 +0800

    Feature: 添加splitChunks和修改gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 
 const Webpack = require("webpack");
 const path = require("path");
-const Gzip = require("compression-webpack-plugin");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+
+const productionGzipExtensions = ["html", "js", "css", "svg"];
 
 function resolve (dir) {
   return path.join(__dirname, dir);
@@ -24,42 +26,88 @@ module.exports = {
   },
   //简单配置
   configureWebpack (config) {
-    if(process.env.NODE_ENV === 'production') {
-      return {
-        plugins: [
-          new Gzip({
-            test: /\.js$|\.html$|\.css$/,
-            threshold: 10240, // 超过10k进行压缩
-            deleteOriginalAssets: false, // 是否删除源文件
-          })
-        ]
-      }
-    }
-    else {
-      return {
-        resolve: {
-          alias: {
-            "@": resolve("src"),
-          },
+
+    return {
+      resolve: {
+        alias: {
+          "@": resolve("src"),
         },
-        plugins: [
-          new Webpack.ProvidePlugin({
-            echarts: "echarts",
-            "window.echarts": "echarts",
-          }),
-        ],
-      };
-    }
+      },
+      plugins: [
+        new Webpack.ProvidePlugin({
+          echarts: "echarts",
+          "window.echarts": "echarts",
+        }),
+      ],
+    };
   },
   // chainWebpack链式配置规则
   // https://github.com/Yatoo2018/webpack-chain/tree/zh-cmn-Hans
   chainWebpack: config => {
-    // GraphQL Loader
-    // 只有在生产过程中添加缩小插件，否则设置devtool到源映射
+    // 去掉preload  prefetch预加载
+    config.plugins.delete("preload");
+    config.plugins.delete("prefetch");
+
+    config.resolve.symlinks(true);
+
     config
       .when(process.env.NODE_ENV === 'production',
-        config => config.plugins.delete('prefetch'),
-        config => config.devtool('source-map')
+
+        config => {
+          config.devtool("none");
+          // 使用CompressionWebpackPlugin gzip压缩文件资源
+          config
+            .plugin("compression")
+            .use(CompressionWebpackPlugin, [
+              {
+                filename: "[path].gz[query]",
+                algorithm: "gzip",
+                test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
+                threshold: 10240, // 超过10k进行压缩
+                deleteOriginalAssets: false, // 是否删除源文件
+                minRatio: 0.8,
+              },
+            ])
+            .end();
+          // html-webpack-plugin的扩展插件，用于给<script>添加async，defer或module属性的元素，甚至内联 //TODO 待研究具体作用
+          // config
+          //   .plugin("ScriptExtHtmlWebpackPlugin")
+          //   .after("html")
+          //   .use("script-ext-html-webpack-plugin", [{ inline: /runtime\..*\.js$/ }])
+          //   .end();
+
+          // splitChunks提取被重复引入的文件，单独生成一个或多个文件，这样避免在多入口重复打包文件
+          config.optimization.splitChunks({
+            chunks: "all",
+            cacheGroups: {
+              libs: {
+                name: "chunk-libs",
+                test: /[\\/]node_modules[\\/]/,
+                priority: 10, //优先级，多个分组冲突时决定把代码放在哪块
+                chunks: "initial",
+              },
+              elementUI: {
+                name: "chunk-elementUI",
+                priority: 20,
+                test: /[\\/]node_modules[\\/]_?element-ui(.*)/,
+              },
+              commons: {
+                name: "chunk-commons",
+                test: resolve("src/components"),
+                minChunks: 3, // 被entry引入的次数，默认1
+                priority: 5,
+                reuseExistingChunk: true,
+              },
+            },
+          });
+          // runtimechunk设置为true会向每个只包含运行时的入口点添加一个额外的块。
+          //为single会创建一个运行时文件，供所有生成的块共享。
+          config.optimization.runtimeChunk("single");
+        },
+        config => {
+          config.devtool('source-map')
+        }
+
       );
 
   }
